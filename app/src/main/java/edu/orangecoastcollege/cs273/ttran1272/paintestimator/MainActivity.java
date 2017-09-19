d00e6bc826eec50cf41ac2f7d4fcb0b1467634bb
Tran <ttran1272@student.cccd.edu>
2017-09-19 15:10:35 -0700

    Initial commit

---
+++ app/src/main/java/edu/orangecoastcollege/cs273/ttran1272/paintestimator/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.orangecoastcollege.cs273.ttran1272.paintestimator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
